181ae698538c74c872cf58209fd2dc4c849f368b
Jazzylol <506496088@qq.com>
2024-11-08 13:11:47 +0800

    first commit

---
+++ src/main/java/com/zfd/bill2db/Bill2dbApplication.java
@@ -0,0 +1,13 @@
+package com.zfd.bill2db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bill2dbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bill2dbApplication.class, args);
+    }
+
+}
