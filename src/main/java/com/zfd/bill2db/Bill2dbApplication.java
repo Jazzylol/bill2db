8002f76c96be5bc86e303230fcd1d2975e4d512a
Jazzylol <506496088@qq.com>
2024-11-14 17:37:04 +0800

    feat: 多环境配置

--- src/main/java/com/zfd/bill2db/Bill2dbApplication.java
+++ src/main/java/com/zfd/bill2db/Bill2dbApplication.java
@@ -1,7 +1,5 @@
 package com.zfd.bill2db;
 
-import com.zfd.bill2db.mapper.TransactionMapper;
-import jakarta.annotation.Resource;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @MapperScan("com.zfd.bill2db.mapper")
 @SpringBootApplication
 public class Bill2dbApplication {
-
 
 
     public static void main(String[] args) {
